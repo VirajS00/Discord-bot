cbfb2afa5402f0ff084556e77f23733e8d9ebfc8
VirajS00 <virajshukla00@gmail.com>
2020-10-13 22:47:57 +0530

    small changes

--- index.js
+++ index.js
@@ -15,12 +15,14 @@ client.on('message', async (msg) => {
 			await msg.channel.send('pong');
 		} else if (msg.content.toLowerCase() === '!8ball') {
 			await msg.channel.send('⓼ ' + eightBall());
-		} else if (msg.content.toLowerCase() === '!sticker') {
+		} else if (msg.content.toLowerCase() === '!sticker' || msg.content.toLowerCase() === '!st') {
 			let rand = Math.floor(Math.random() * 7) + 1;
 			let attachment = new MessageAttachment('./friendss/' + rand + '.webp');
 			await msg.channel.send(attachment);
 		} else if (msg.content.toLowerCase() === '!gif' || /!gif\s(.*)/i.test(msg.content)) {
-			await msg.channel.send(await giphyGet(msg.content.toLowerCase()).catch((err) => 'cannot get GIF'));
+			await msg.channel.send(
+				await giphyGet(msg.content.toLowerCase()).catch((err) => '```diff\n- cannot get GIF\n```')
+			);
 		} else if (msg.content.toLowerCase() === '!react' || /!react\s(.*)/i.test(msg.content)) {
 			await msg.react(reaction(msg.content.toLowerCase()));
 		}
